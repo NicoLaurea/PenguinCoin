a363a7e8b4bdc41741e8f9937bbc84e8065f094c
NicoLaurea <83653381+NicoLaurea@users.noreply.github.com>
2024-03-02 13:40:16 +0000

    first commit

---
+++ Pengu20/scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const lock = await ethers.deployContract("PNG", ["0x79F01bf6dD6EE7E0daE68BFfD888dd784b82070A"]); //wallet add
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Token deployed to ${lock.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
